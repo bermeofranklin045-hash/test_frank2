cb9d66f4762009b731d69125c5fb2c6e51c26fbd
Franklin <bermeofranklin045@gmail.com>
2026-04-16 18:16:14 -0500

    I'll leave programming all day tomorrow; we'll start with the new library :)

--- script.js
+++ script.js
@@ -1,18 +1,73 @@
-// const botones = document.querySelectorAll('.button-apply-job');
-// botones.forEach(function (boton) {
-//     boton.addEventListener('click', function () {
-//         boton.textContent = '¡Aplicado!';
-//         boton.classList.add('is-applied');
-//         boton.disabled = true;
-//     });
-// });
+import './devjobs-avatar-elements.js' 
 
 const jobListingSection = document.querySelector('.jobs-listing-card');
- jobListingSection.addEventListener('click', function (event) {
+
+jobListingSection.addEventListener('click', function (event) {
     const element = event.target;
+
+    // 👉 Si hace click en el botón
     if (element.classList.contains('button-apply-job')) {
         element.textContent = '¡Aplicado!';
         element.classList.add('is-applied');
         element.disabled = true;
+    } else {
+        window.location.href = "infoempleo.html";
     }
 });
+
+
+const filter = document.querySelector('#filter-location')
+const mensaje = document.querySelector('#filter-selected-value')
+
+filter.addEventListener('change', function () {
+    const selectedValue = filter.value;
+    const jobs = document.querySelectorAll('.job-listing-card')
+
+    if (selectedValue) {
+        mensaje.textContent = `Has seleccionado: ${selectedValue}`
+    } else {
+        mensaje.textContent = '';
+    }
+
+    
+    jobs.forEach(job => {
+        const modalidad = job.dataset.modalidad
+        if (selectedValue === '' || selectedValue === modalidad) {
+            job.style.display = 'block'
+
+        } else {
+            job.style.display = 'none'
+        }
+    })
+})
+
+
+
+
+
+const container = document.querySelector('.jobs-listing-card')
+fetch("./data.json")
+  .then((response)=> {
+    return response.json();
+  })
+  .then((jobs)=>{
+    jobs.forEach(job => {
+     const article = document.createElement('article')
+      
+      article.className='job-listing-card'
+
+      article.dataset.modalidad = job.data.modalidad
+      article.dataset.nivel = job.data.nivel
+      article.dataset.technology = job.data.technology
+
+       article.innerHTML = `<div>
+          <h3>${job.titulo}</h3>
+          <small>${job.empresa} | ${job.ubicacion}</small>
+          <p>${job.descripcion}</p>
+        </div>
+        <button class="btn-aplicar button-apply-job" >Aplicar</button>`
+
+      container.appendChild(article)
+    })
+ })
+
